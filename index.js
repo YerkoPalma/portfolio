a2ade97eb34b61f63d555262af672d45791d91d9
Yerko Palma <yerko.palma@usach.cl>
2017-05-08 20:08:22 +0000

    handle simple animations for page transitions

--- index.js
+++ index.js
@@ -88,13 +88,31 @@ function notFoundView (params, state) {
 
 function onRender (currentView, previousView) {
   // make dissappear the previousView
-  if (previousView && previousView.classList) previousView.classList.add('o-0')
   previousView.addEventListener('transitionend', function (e) {
-    if (currentView && currentView.classList) currentView.classList.add('o-0')
-    e.stopPropagation()
+    if (currentView && currentView.classList) {
+      // currentView.classList.add('t')
+      currentView.style.transition = 'all 0.5s'
+      currentView.classList.add('o-0')
+    }
+    // e.stopPropagation()
     // replace it with the currentView
-    router.rootEl.replaceChild(currentView, previousView)
+    router.rootEl.appendChild(currentView)
+    router.rootEl.removeChild(previousView)
     // make it appear
     if (currentView && currentView.classList) currentView.classList.remove('o-0')
+
+    var links = document.querySelectorAll('a[data-route]')
+    Array.prototype.forEach.call(links, function (link) {
+      link.addEventListener('click', function (e) {
+        e.preventDefault()
+        if (!link.getAttribute('data-bind')) router.goToPath(link.getAttribute('data-route'))
+        if (typeof router.onNavClick === 'function') router.onNavClick(link.getAttribute('data-route'), link)
+      })
+    })
   })
+  if (previousView && previousView.classList) {
+    // previousView.classList.add('t')
+    previousView.style.transition = 'all 0.5s'
+    previousView.classList.add('o-0')
+  }
 }
